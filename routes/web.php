502048b07ecdd63101adeb4e4e577d4b1d99430d
Pape Samba NDOUR <papesambandour@hotmail.com>
2022-08-01 03:05:46 +0000

    finish mvp

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-   dd('DTYB7FDGNHHFT', \Illuminate\Support\Facades\Hash::make('DTYB7FDGNHHFT'));
     return redirect('/partner');
 });
 
